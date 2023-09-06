0d0994e5755a3ad2e728d7596674fbeeae8b64f0
Antonio Rodriguez <joserdz052@outlook.com>
2023-09-06 15:36:08 -0600

    ADD model Vue
    add structure like vue

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Main extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -20,6 +20,9 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome');
+		//las partes de $this->load->view('nombre_vista',$variable_enviada,true o false(true si queremos que se guarde en una variable la vista))
+        $data['main']=$this->load->view('ejemplo','',true);
+		//Cargo vista general donde esta el layout_general
+		$this->load->view('plantilla',$data);
 	}
 }
--- application/views/plantilla.php
+++ application/views/plantilla.php
@@ -4,17 +4,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="es">
 <head>
 	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
+	<title>Compensapay</title>
 <!-- importamos materialize usando base_url() -->
 <link rel="stylesheet" href="<?php echo base_url();?>css/materialize.min.css">
 </head>
 <body>
-<div class="collection">
-    <a href="#!" class="collection-item"><span class="badge">1</span>Alan</a>
-    <a href="#!" class="collection-item"><span class="new badge">4</span>Alan</a>
-    <a href="#!" class="collection-item">Alan</a>
-    <a href="#!" class="collection-item"><span class="badge">14</span>Alan</a>
-  </div>
+
+
+    <?php
+    if (isset($main)) {
+        echo $main;
+    }
+
+    ?>
+
 
 <!-- Ejecutor de scripts de materialize -->
 <script src="<?php echo base_url();?>js/materialize.min.js"></script>
