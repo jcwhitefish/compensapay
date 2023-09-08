d1db94eae3488a5579bd0513d5eacdc0688abeae
Antonio Rodriguez <joserdz052@outlook.com>
2023-09-08 08:00:38 -0600

    ADD empresa
    add empresas.php view

--- application/views/registro/empresa.php
+++ application/views/registro/empresa.php
@@ -2,27 +2,65 @@
     <div class="card esquinasRedondas">
         <div class="card-content">
             <h2 class="card-title regEmpresa bold">Registro de Empresa</h2>
-            <div class="row">
-                <div class="col l7">
-                    <div class="col l12 detEmpresa">
-                        <p class="bold">Detalles de la empresa</p>
+
+            <form method="post" action="<?php echo site_url('controladorcontroller/save_xd'); ?>" class="col s12" enctype="multipart/form-data">
+                <div class="row">
+                    <div class="col l6" >
+                        <div class="row">
+
+                            <div class="col l12 detEmpresa">
+                                <p class="bold">Detalles de la empresa</p>
+                            </div>
+                            <div class="input-border col l5">
+                                <input type="text" name="user" id="user" required>
+                                <label class="bold" for="user">Razon Social</label>
+                            </div>
+                            <div class="input-border col l5 offset-l2">
+                                <select name="profile" id="profile" required>
+                                    <option value="perfil1">Perfil 1</option>
+                                    <option value="perfil2">Perfil 2</option>
+                                    <option value="perfil3">Perfil 3</option>
+                                </select>
+                                <label class="bold">Giro</label>
+                            </div>
+                            <div class="input-border col l12">
+                                <input type="text" name="user" id="user" required>
+                                <label class="bold" for="user">Nombre Comercial</label>
+                            </div>
+                            <div class="input-border col l5">
+                                <input type="text" name="user" id="user" required>
+                                <label class="bold" for="user">RFC</label>
+                            </div>
+                            <div class="input-border col l5 offset-l2">
+                                <select name="profile" id="profile" required>
+                                    <option value="perfil1">Perfil 1</option>
+                                    <option value="perfil2">Perfil 2</option>
+                                    <option value="perfil3">Perfil 3</option>
+                                </select>
+                                <label class="bold">Regimen Fiscal</label>
+                            </div>
+
+                        </div>
                     </div>
-                    <div class="input-border col l5">
-                        <input type="text" name="user" id="user" required>
-                        <label class="bold" for="user">Razon Social</label>
+                    <div class="col l1"></div>
+                    <div class="col l3">
+                        <div class="col l12 detEmpresa">
+                            <p class="bold">Datos Bancarios</p>
+                        </div>
+                        <div class="input-border col l12">
+                            <input type="text" name="user" id="user" required>
+                            <label class="bold" for="user">Cuenta Clabe</label>
+                        </div>
+                        <div class="input-border col l12">
+                            <input type="text" name="user" id="user" required>
+                            <label class="bold" for="user">Banco Emisor</label>
+                        </div>
                     </div>
-                    <div class="input-border col l5 offset-l2">
-                        <input type="text" name="user" id="user" required>
-                        <label class="bold" for="user">Razon Social</label>
+                    <div class="col l2">
+                        hola
                     </div>
                 </div>
-                <div class="col l3">
-                    hola
-                </div>
-                <div class="col l2">
-                    hola
-                </div>
-            </div>
+            </form>
         </div>
     </div>
 </div>
@@ -35,12 +73,10 @@
     .esquinasRedondas {
         border-radius: 25px !important;
     }
-    .detEmpresa{
+
+    .detEmpresa {
         margin-left: 40px;
         margin-bottom: 30px;
-    }
-    .regEmpresa{
-        margin-left: 20px;
     }
 
 </style>
