e5e07d557669e4e251c4dc9f21b9e7096eca455f
Israel Hernández Solorio <jisraelhs@gmail.com>
2023-09-13 17:00:43 -0600

    FIX  - Salidas de revisión, salida correcta

--- application/models/Interaccionbd.php
+++ application/models/Interaccionbd.php
@@ -21,22 +21,23 @@ class Interaccionbd extends CI_Model
         if ($regreso)
         {
            $tempo = $regreso->result_array()[0]['existe'];
-            echo $tempo;
+         //   echo $tempo;
             if ($tempo == 0)
             {
-                echo "No hay datos";
+           //     echo "No hay datos";
+            
             }
             else
             {
-                echo "Regreso distinto a 0";
+             //   echo "Regreso distinto a 0";
             }
         }
         else
         {
-            echo "Algo fallo";
+            $tempo=500;//echo "Algo fallo";
         }
        
-       
+       return $tempo;
     }
 
 
