53f85995a7a6d1135fc649b4099a6357263dd4da
AbdielMg007 <abdiel.mugo@gmail.com>
2023-09-13 11:11:11 -0600

    ADD assets
    estrcutura  de la carpeta de assets

---
+++ assets/js/storeRegistro.js
@@ -0,0 +1,16 @@
+const store = Vuex.createStore({
+  state() {
+      return {
+          counter: 0
+      };
+  },
+  mutations: {
+    // Mutaciones para actualizar el estado si es necesario
+  },
+  actions: {
+    // Acciones para realizar operaciones asíncronas si es necesario
+  },
+  getters: {
+    // Getters para acceder al estado de manera calculada si es necesario
+  },
+});
