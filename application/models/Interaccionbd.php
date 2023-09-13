9c9c21367aed92d5bead4b08849802f3d0f3f062
Israel Hernández Solorio <jisraelhs@gmail.com>
2023-09-13 16:55:45 -0600

    ADD - Modulo para interaccion con base de datos

---
+++ application/models/Interaccionbd.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Description of DataBase Interaction
+ * @author JIsraelHS
+ * @email  israel.hernandez@whitefish.mx
+ */
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Interaccionbd extends CI_Model
+{
+	// declare private variable
+    private $sql;
+    private $regreso;
+    private $tempo;
+
+    public function ExisteUsuario($usuario)
+    {
+       $sql="select existeUsuario('".$usuario."','valor llave') as existe;";
+        $regreso = $this->db->query ($sql); 
+        if ($regreso)
+        {
+           $tempo = $regreso->result_array()[0]['existe'];
+            echo $tempo;
+            if ($tempo == 0)
+            {
+                echo "No hay datos";
+            }
+            else
+            {
+                echo "Regreso distinto a 0";
+            }
+        }
+        else
+        {
+            echo "Algo fallo";
+        }
+       
+       
+    }
+
+
+}
