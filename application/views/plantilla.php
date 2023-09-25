d65ad8faa8a111bc30cc6a7ff70ec5720284dfad
AbdielMg007 <abdiel.mugo@gmail.com>
2023-09-25 17:03:18 -0600

    ADD Interface invoices
    se crea el objeto y la interfaz de las facturas

--- application/views/plantilla.php
+++ application/views/plantilla.php
@@ -20,23 +20,26 @@ defined('BASEPATH') or exit('No direct script access allowed');
         <?php
 
          $navbar=true;
-         $sidebar=false;
-         $isLog=false;
+         $sidebar=true;
+         $isLog=true;
 
         if($navbar) {
         ?>
             <nav>
                 <div class="nav nav-wrapper">
                     <img src="<?= base_url('assets/images/CompensaPay_Logos-01.png'); ?>" alt="Logo" class="custom-image">
+                    <!-- If the user is log show the $navbar complements -->
                     <?php if ($isLog): ?> 
+                        <!-- If the user has a photo show that if not show the name -->
                         <?php if (true): ?>
                             <img src="<?= base_url('assets/images/CompensaPay_Logos-02.png'); ?>" alt="Logo" class="custom-image image-center hide-on-med-and-down">
                         <?php else: ?>
                             <h4 class="image-center p-3 hide-on-med-and-down">Name</h4>
                         <?php endif; ?>
-                        <ul class="right hide-on-med-and-down px-3">
-                            <li class=""><a href="#"><i class="material-icons p-1">notifications_none</i></a></li>
-                        </ul>
+                        <div class="right hide-on-med-and-down px-3">
+                            <label class="px-3">Balance</label>
+                            <p class="nav-price"><a href="">$200.000</a></p>
+                        </div>
                         <div class="right hide-on-med-and-down px-3">
                             <p class="right-align">Display Name</p>
                             <select name="type" id="type" class="browser-default input-nav">
@@ -57,16 +60,17 @@ defined('BASEPATH') or exit('No direct script access allowed');
                 <hr class="line-side">
                 <ul>
                 <ul class="icon-list">
-                    <li><a href="#"><i class="Tiny material-icons p-1">search</i></a></li>
-                    <li><a href="#"><i class="Tiny material-icons p-1">home</i></a></li>
-                    <li><a href="#"><i class="Tiny material-icons p-1">import_export</i></a></li>
-                    <li><a href="#"><i class="Tiny material-icons p-1">pie_chart</i></a></li>
-                    <li><a href="#"><i class="Tiny material-icons p-1">mail</i></a></li>
-                    <li><a href="#"><i class="Tiny material-icons p-1">collections</i></a></li>
-                    <li><a href="#"><i class="Tiny material-icons p-1">today</i></a></li>
-                    <li><a href="#"><i class="Tiny material-icons p-1">people</i></a></li>
-                    <li><a href="#"><i class="Tiny material-icons p-1">settings</i></a></li>
-                    <li><a href="#"><i class="Tiny material-icons p-1">headset_mic</i></a></li>
+                    <li><a href="#"><i class="tiny material-icons p-1">notifications_none</i></a></li>
+                    <li><a href="#"><i class="tiny material-icons p-1">home</i></a></li>
+                    <li><a href="#"><i class="tiny material-icons p-1">import_export</i></a></li>
+                    <li><a href="#"><i class="tiny material-icons p-1">pie_chart</i></a></li>
+                    <li><a href="#"><i class="tiny material-icons p-1">mail</i></a></li>
+                    <li><a href="#"><i class="tiny material-icons p-1">collections</i></a></li>
+                    <li><a href="#"><i class="tiny material-icons p-1">today</i></a></li>
+                    <li><a href="#"><i class="tiny material-icons p-1">people</i></a></li>
+                    <li><a href="#"><i class="tiny material-icons p-1">settings</i></a></li>
+                    <li><a href="#"><i class="tiny material-icons p-1">headset_mic</i></a></li>
+                    <li><a href="#"><i class="tiny material-icons p-1">exit_to_app</i></a></li>
                 </ul>
             </div>
             <div class="container-main">
@@ -127,6 +131,9 @@ defined('BASEPATH') or exit('No direct script access allowed');
     }
     .icon-list li a:hover i {
         color: #e0e51d;
+        background-color: #fff; /* Fondo blanco */
+        border-radius: 60%; /* Borde redondeado para crear un círculo */
+        padding: 10px;
     }
 
     /* Padding si se tiene la barra lateral */
@@ -151,9 +158,15 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
     .nav p{
         color: #888;
-        margin: 0;
+        margin: 0 ;
         padding: 0;
         margin-top: -15px;
+    }
+    .nav-price{
+        margin: 0 !important;
+        padding: 5px !important;
+        margin-top: -45px !important;
+        text-decoration: underline;
     }
 
     .image-center{
@@ -170,10 +183,10 @@ defined('BASEPATH') or exit('No direct script access allowed');
     }
 
     #header{
-  position: fixed;
-  width: 100%;
-  z-index: 1000;
-}
+        position: fixed;
+        width: 100%;
+        z-index: 1000;
+    }
 
 </style>
 
