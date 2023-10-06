8edcf4d9c71a1e1c1994cd4ccaed0a5e8a9b74b9
AbdielMg007 <abdiel.mugo@gmail.com>
2023-10-06 13:14:56 -0600

    FIX data table

--- application/views/facturas/proveedor.php
+++ application/views/facturas/proveedor.php
@@ -1,13 +1,15 @@
 
 <div class="p-5" id="app">
+
+
     <div class="row">
         <p class="px-3">Periodo:</p>
         <div class="col l3">
-            <input type="date" id="start" name="trip-start" value="2018-07-22" min="2018-01-01" max="2018-12-31" />
+            <input type="date" id="start" name="trip-start" value="2023-07-22" min="2023-01-01" max="2040-12-31" />
             <label for="start">Inicio:</label>
         </div>
         <div class="col l3">
-            <input type="date" id="fin" name="trip-start" value="2018-07-22" min="2018-01-01" max="2018-12-31" />
+            <input type="date" id="fin" name="trip-start" value="2023-07-22" min="2023-01-01" max="2040-12-31" />
             <label for="fin">Fin:</label>
         </div>
         <div class="col l6 right-align p-5">
@@ -61,16 +63,16 @@
                         <?php $facturas = array_reverse($facturas);
                             foreach ($facturas as $row) : ?>
                             <tr>
-                                <td class="tabla-celda">
+                                <td class="tabla-celda center-align">
                                     <?php
                                     if ($row->o_Activo == 0) {
                                         echo '<a href="#">Crear Operación</a>';
                                     } elseif ($row->o_Activo == 1) {
-                                        echo '<i class="tiny material-icons" style="color: green;">check_circle</i>';
+                                        echo '<i class="small material-icons" style="color: green;">check_circle</i>';
                                     }
                                     ?>
                                 </td>             
-                                <td class="tabla-celda"><a href="#">Empresa 1</a></td><!--aqui deberia estar usuario -->
+                                <td class="tabla-celda"><a href="#">Frontier</a></td><!--aqui deberia estar usuario -->
                                 <td class="tabla-celda"><?= $row->o_NumOperacion ?></td><!--aqui deberia estar row -->
                                 <td class="tabla-celda">{{modificarFecha('<?= $row->o_FechaEmision ?>')}}</td><!--aqui deberia estar las fechas bien -->
                                 <td class="tabla-celda">{{modificarFecha('<?= $row->o_FechaUpload ?>')}}</td>
@@ -84,9 +86,9 @@
                                     }
                                     ?>
                                 </td>   
-                                <td class="tabla-celda">$<?= $row->o_SubTotal ?></td>
-                                <td class="tabla-celda">$<?= $row->o_Impuesto ?></td>
-                                <td class="tabla-celda">$<?= $row->o_Total ?></td>
+                                <td class="tabla-celda">$<?= number_format($row->o_SubTotal); ?></td>
+                                <td class="tabla-celda">$<?= number_format($row->o_Impuesto); ?></td>
+                                <td class="tabla-celda">$<?= number_format($row->o_Total); ?></td>
                             </tr>
                         <?php endforeach; ?>
                     </tbody>
@@ -113,26 +115,26 @@
                         <?php $operaciones = array_reverse($operaciones);
                             foreach ($operaciones as $operacion) : ?>
                             <tr>
-                                <td class="tabla-celda">
+                                <td class="tabla-celda center-align">
                                     <?php
                                     if ($operacion->Aprobacion == 0) {
                                         echo '';
                                     } elseif ($operacion->Aprobacion == 1) {
-                                        echo '<i class="tiny material-icons" style="color: green;">check_circle</i>';
+                                        echo '<i class="small material-icons" style="color: green;">check_circle</i>';
                                     }
                                     ?>
                                 </td>
                                 <td class="tabla-celda"><?php echo $operacion->ID_Operacion; ?></td>
-                                <td class="tabla-celda"><?php echo $operacion->Proveedor; ?></td>
+                                <td class="tabla-celda"><a><?php echo $operacion->Proveedor; ?></a></td>
                                 <td class="tabla-celda"><?php echo $operacion->Fecha_Factura; ?></td>
                                 <td class="tabla-celda"><?php echo $operacion->Fecha_Alta; ?></td>
                                 <td class="tabla-celda"><?php echo $operacion->Factura; ?></td>
-                                <td class="tabla-celda"><?php echo $operacion->Nota_Debito_Factura_Proveedor; ?></td>
-                                <td class="tabla-celda"><?php echo $operacion->Fecha_Nota_Debito_Fact_Proveedor; ?></td>
+                                <td class="tabla-celda"><?php echo $operacion->Nota_Debito_Factura_Proveedor !== null ? $operacion->Nota_Debito_Factura_Proveedor : 'N/A'; ?></td>
+                                <td class="tabla-celda"><?php echo $operacion->Nota_Debito_Factura_Proveedor !== null ? $operacion->Nota_Debito_Factura_Proveedor : 'N/A'; ?></td>
                                 <td class="tabla-celda"><?php echo $operacion->Fecha_Transaccion; ?></td>
                                 <td class="tabla-celda"><?php echo $operacion->Estatus; ?></td>
-                                <td class="tabla-celda"><?php echo $operacion->Monto_Ingreso; ?></td>
-                                <td class="tabla-celda"><?php echo $operacion->Monto_Egreso; ?></td>
+                                <td class="tabla-celda">$<?php echo number_format($operacion->Monto_Ingreso); ?></td>
+                                <td class="tabla-celda">$<?php echo number_format( $operacion->Monto_Egreso); ?></td>
                                 <td class="tabla-celda">
                                     <?php
                                     if ($operacion->Aprobacion == 0) {
@@ -172,17 +174,22 @@
                                 <label for="invoiceUpload" class="custom-file-upload button-blue">Seleccionar</label>
                                 <input @change="checkFormatInvoice" name="invoiceUpload" ref="invoiceUpload" id="invoiceUpload" type="file" accept="application/xml" maxFileSize="5242880" required />
                             </div><br>
+                            <div class="col l10">
+                                <p class="text-modal" :style="{ color: isTextRed ? 'red' : '' }">
+                                    *Al dar clic en "crear operación", el proveedor acepta que al concluir la transacción por el pago de la factura,
+                                    se descontará y enviará al cliente de forma automática el monto de la nota de débito o factura del cliente,
+                                    de acuerdo con lo estipulado en nuestros términos y condiciones
+                                </p><br>
+                            </div>
+                            <div class="col l2 center-align">
+                                <input type="checkbox" v-model="checkboxChecked" required>
+                            </div>
                             <div class="col l12 center-align">
                                 <a class="modal-close button-gray" style="color: #fff; color:hover: #"
                                 >Cancelar</a
                                 >
                                 &nbsp;
-                                <button class="button-blue" type="button" name="action" @click="uploadFile">Siguiente</button><br /><br />
-                                <p class="text-modal">
-                                *Al dar clic en "crear operación", el proveedor acepta que al concluir la transacción por el pago de la factura,
-                                se descontará y enviará al cliente de forma automática el monto de la nota de débito o factura del cliente,
-                                de acuerdo con lo estipulado en nuestros términos y condiciones
-                                </p>
+                                <button class="button-blue" type="button" name="action" @click="uploadFile">Siguiente</button>
                             </div>
                         </div>
                     </form>
@@ -197,15 +204,14 @@
     <div id="modal-operacion" class="modal">
         <div class="modal-content">
             <h5>Crear Operación</h5>
-            
             <div class="card esquinasRedondas">
                 <div class="card-content">
-                    <h6 class="p-3">Carga tu factura y selecciona una factura del proveedor o busca un proveedor y selecciona una factura</h6>
+                    <h6 class="p-3">Carga tu nota de debito y selecciona una factura del proveedor</h6>
                     <form method="post" action="<?php echo base_url('facturas/carga'); ?>" enctype="multipart/form-data">
                         <div class="row">
                             <div class="col l3 input-border">
                                 <input type="text" name="invoiceDisabled" id="invoiceDisabled" disabled v-model="invoiceUploadName">
-                                <label for="invoiceDisabled">Tu factura XML</label>
+                                <label for="invoiceDisabled">Tu Nota de debito en XML</label>
                             </div>
                             <div class="col l4 left-align p-5">
                                 <label for="invoiceUpload" class="custom-file-upload button-blue">Seleccionar</label>
@@ -213,7 +219,7 @@
                             </div>
                             <div class="col l5 input-border select-white">
                                 <input type="text" name="providerDisabled" id="providerDisabled" disabled v-model="providerUploadName">
-                                <label for="providerDisabled">Proveedor</label>
+                                <label for="providerDisabled">Cliente</label>
                             </div>
                             <div>
                                 <table class="striped">
@@ -235,16 +241,16 @@
                                         <?php foreach ($facturas as $row) : ?>
                                             <?php if ($row->o_Activo == 0) : ?> <!-- Verificar si o_Activo es igual a 0 -->
                                                 <tr>
-                                                    <td class="tabla-celda"><input type="radio" name="grupo" value="opcion1"></td>
+                                                    <td class="tabla-celda center-align"><input type="radio" name="grupo" value="opcion1" v-model="checkboxChecked" required></td>
                                                     <td class="tabla-celda">Frontier</td><!-- Aquí debería estar usuario -->
                                                     <td class="tabla-celda"><?= $row->o_NumOperacion ?></td><!-- Aquí debería estar row -->
                                                     <td class="tabla-celda"><?= $row->o_FechaEmision ?></td><!-- Aquí debería estar las fechas bien -->
                                                     <td class="tabla-celda"><?= $row->o_FechaUpload ?></td>
                                                     <td class="tabla-celda"><?= $row->o_FechaEmision ?></td>
                                                     <td class="tabla-celda">Pendiente</td>
-                                                    <td class="tabla-celda">$<?= $row->o_SubTotal ?></td>
-                                                    <td class="tabla-celda">$<?= $row->o_Impuesto ?></td>
-                                                    <td class="tabla-celda">$<?= $row->o_Total ?></td>
+                                                    <td class="tabla-celda">$<?= number_format($row->o_SubTotal); ?></td>
+                                                    <td class="tabla-celda">$<?= number_format($row->o_Impuesto); ?></td>
+                                                    <td class="tabla-celda">$<?= number_format($row->o_Total); ?></td>
                                                 </tr>
                                             <?php endif; ?>
                                         <?php endforeach; ?>
@@ -305,7 +311,6 @@
         min-width: 100px;
         max-width: 150px; 
         padding: 5px; 
-        text-align: center; 
         font-size: 13px;
         overflow: hidden;
         text-overflow: ellipsis;
@@ -348,6 +353,10 @@
         pointer-events: auto;
     }
 
+    .text-error {
+        color: red;
+    }
+
     
    
 </style>
@@ -358,6 +367,8 @@
             const invoiceUploadName = Vue.ref('');
             const providerUploadName = Vue.ref('');
             const selectedButton = Vue.ref('Operaciones');
+            const checkboxChecked = Vue.ref(false);
+            const isTextRed = Vue.computed(() => selectedButton.value === 'Facturas' && !checkboxChecked.value);
 
             const checkFormatInvoice = (event) => {
                 const fileInput = event.target;
@@ -372,27 +383,26 @@
 
 
             const uploadFile = async () => {
-                if (selectedButton.value === 'Facturas') {
-                    
+                if (selectedButton.value === 'Facturas' && checkboxChecked.value) {
                     const fileInput = document.getElementById('invoiceUpload');
                     const formData = new FormData();
                     formData.append('user', 6);
                     formData.append('invoiceUpload', fileInput.files[0]);
 
-                 
-                        const response = await fetch("<?= base_url('facturas/subida') ?>", {
-                            method: 'POST',
-                            body: formData,
-                            redirect: 'follow'
-                        });
+                    const response = await fetch("<?= base_url('facturas/subida') ?>", {
+                        method: 'POST',
+                        body: formData,
+                        redirect: 'follow'
+                    });
 
-                        if (response.ok) {
-                            console.log('se subio');
-                            window.location.href = '<?php base_url('facturas') ?>';
-                        } else {
-                            console.error('Error');
-                        }
-
+                    if (response.ok) {
+                        console.log('se subió');
+                        window.location.href = '<?php base_url('facturas') ?>';
+                    } else {
+                        console.error('Error');
+                    }
+                }else{
+                    alert('Ingresa una factura y acepta los terminos')
                 }
             };
 
@@ -402,7 +412,7 @@
                     fecha[1] = '';
                     fecha = fecha.join(' ');
                     return fecha;
-                }
+                };
 
             const selectButton = (buttonName) => {
                 if (selectedButton.value == buttonName) {
@@ -417,9 +427,11 @@
                 providerUploadName,
                 selectedButton,
                 checkFormatInvoice,
+                checkboxChecked,
                 uploadFile,
                 modificarFecha,
                 selectButton,
+                isTextRed,
             };
         }
     }); 
