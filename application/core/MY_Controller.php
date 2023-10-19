ed6a5eb50349a54f1e30334acce3f32126fd1e12
Antonio Rodriguez <joserdz052@outlook.com>
2023-10-19 15:26:22 -0600

    FIX navbar
    complete nav

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -9,8 +9,10 @@ class MY_Controller extends CI_Controller
     }
 }
 // Este es para las paginas que necesitas estar logueado
-class MY_Loggedin extends CI_Controller {
-    public function __construct() {
+class MY_Loggedin extends CI_Controller
+{
+    public function __construct()
+    {
         parent::__construct();
         $this->load->library('session');
         $this->load->model('user_model');
@@ -19,27 +21,30 @@ class MY_Loggedin extends CI_Controller {
         // Verificar si el usuario ha iniciado sesión en todas las páginas protegidas
         if ($this->session->userdata('logged_in')) {
             //consultamos los datos del usuario cada que se recarga la pagina
-            $data['datosUsuario']  = $this->user_model->get_user(array('id' => $this->session->userdata('id')));
-            // var_dump($data['datosUsuario']);
-            //consultamos los datos de la empresa
-            $data['datosEmpresa']  = $this->company_model->get_company(array('id' => $data['datosUsuario']['id_company']));
-            //var_dump($data['datosEmpresa']);
-        }else{
+            $this->session->set_userdata('datosUsuario', $this->user_model->get_user(array('id' => $this->session->userdata('id'))));
+
+            // var_dump($this->session->userdata('datosUsuario'));
+            //consultamos los datos de la empresa con $this->session->set_userdata('datosUsuario',); el id de la empresa
+            $this->session->set_userdata('datosEmpresa',$this->company_model->get_company(array('id' => $this->session->userdata('datosUsuario')['id_company'])));
+
+            //var_dump($this->session->userdata('datosEmpresa'));
+        } else {
             redirect(base_url('login')); // Redirigir al inicio de sesión si no está autenticado
         }
     }
 }
 // Este es para las paginas que NO tienes que estar logueado
-class MY_Loggedout extends CI_Controller {
-    public function __construct() {
+class MY_Loggedout extends CI_Controller
+{
+    public function __construct()
+    {
         parent::__construct();
         $this->load->library('session');
-        
+
         // Verificar si el usuario ha iniciado sesión en todas las páginas protegidas
         if ($this->session->userdata('logged_in')) {
             redirect(base_url('inicio')); // Redirigir al Dashboard
-        }else{
-            
+        } else {
         }
     }
 }
--- application/views/plantilla.php
+++ application/views/plantilla.php
@@ -1,5 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
+$urlEmpresa = base_url('boveda/' . $this->session->userdata('datosEmpresa')['unique_key'] . '/' . $this->session->userdata('datosEmpresa')['unique_key'] . '-');
+$urlUsuario = base_url('boveda/' . $this->session->userdata('datosUsuario')['unique_key'] . '/' . $this->session->userdata('datosUsuario')['unique_key'] . '-');
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -22,8 +24,9 @@ defined('BASEPATH') or exit('No direct script access allowed');
     <script src="<?php echo base_url('assets/js/materialize.min.js'); ?>"></script>
     <!-- Insertamos el nav -->
     <nav class="main-nav">
-        <div class="nav navegador d-flex">
+        <div class="nav nav-wrapper navegador d-flex">
             <div class="d-flex justificarVertical">
+
                 <img src="<?= base_url('assets/images/CompensaPay_Logos-01.png'); ?>" alt="Logo" class="nav-image">
             </div>
 
@@ -32,23 +35,27 @@ defined('BASEPATH') or exit('No direct script access allowed');
             <?php if ($this->session->userdata('logged_in')) : ?>
                 <div class="d-flex justificarVertical">
                     <!-- If the user has a photo show that if not show the name -->
-                    <div class="logoEmpresa d-flex justificarVertical" id="logoEmpresa"></div>
+                    <div class="logoEmpresa d-flex justificarVertical">
+                        <img id="logoValidar" src="<?= $urlEmpresa . 'logo.jpeg'; ?>" alt="Logo" class="logoEmpresa image-center hide-on-med-and-down">
+
+                    </div>
                 </div>
-                <div class="d-flex justificarVertical">
-                    <div class="hide-on-med-and-down">
-                        <label class="px-3">Balance</label>
-                        <p class="nav-price"><a href="">$200.000</a></p>
+                <div class="d-flex justificarVertical" style="height: 80px!important;">
+                    <div class="hide-on-med-and-down d-flex flex-direction-column justificarVertical">
+                        <p class="balance nav-max">Balance</p>
+                        <a class="nav-max" style="top:-25px;" href="">$200.000</a>
                     </div>
                     <div class="hide-on-med-and-down d-flex flex-direction-column justificarVertical">
-                        <p style="padding-left: 10px;">Noe Salgado</p>
-                        <select onchange="cambiarVista(this.value)" name="type" id="type" class="browser-default input-nav">
+                        <p class="nav-max" style="padding-left: 10px;top:-15px"><?= $this->session->userdata('datosUsuario')['user'] ?></p>
+                        <select onchange="cambiarVista(this.value)" name="type" id="type" class="nav-max browser-default input-nav" style="top: -2px;">
                             <!-- cliente 1 provedor 2 -->
                             <option value="1" <?= $this->session->userdata('vista') == 1 ? 'selected' : '' ?>>Vista Cliente</option>
                             <option value="2" <?= $this->session->userdata('vista') == 2 ? 'selected' : '' ?>>Vista Proveedor</option>
                         </select>
                     </div>
                     <div class="d-flex justificarVertical alinearVertical">
-                        <img src="<?= base_url('assets/images/mark-zuckerberg-bio.png'); ?>" alt="Logo" class="custom-logo hide-on-med-and-down logoUsuario">
+
+                        <img src="<?= $urlUsuario . 'foto.jpeg' ?>" alt="Logo" class="custom-logo hide-on-med-and-down logoUsuario">
                     </div>
                 </div>
             <?php endif; ?>
@@ -58,42 +65,85 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
         </div>
     </nav>
+    <?php if ($this->session->userdata('logged_in')) : ?>
+        <div class="sidebar center-align">
+            <a href="<?= base_url('') ?>"><img src="<?= base_url('assets/images/CompensaPay_Logos-04.png'); ?>" alt="Logo" class="image-side hide-on-med-and-down"></a>
+            <hr class="line-side">
+            <ul>
+                <ul class="icon-list">
+                    <?php echo sprintf('<li><a href="%s"><i class="material-icons%s">notifications</i></a></li>', base_url('notificaciones'), (strpos(current_url(), 'notificaciones')) ? ' icon-list-hover' : ''); ?>
+                    <?php echo sprintf('<li><a href="%s"><i class="material-icons%s">home</i></a></li>', base_url('inicio'), (count(array_intersect(['notificaciones', 'facturas', 'calendario', 'clientesproveedores', 'perfil', 'soporte', 'modelofiscal', 'xml'], explode('/', current_url()))) == 0) ? ' icon-list-hover' : ''); ?>
+                    <?php echo sprintf('<li><a href="%s"><i class="material-icons%s">swap_horiz</i></a></li>', base_url('facturas'), (strpos(current_url(), 'facturas') !== false || strpos(current_url(), 'facturas/subida') !== false) ? ' icon-list-hover' : ''); ?>
+                    <!-- <?php echo sprintf('<li><a href="%s"><i class="material-icons%s">pie_chart</i></a></li>', base_url('xml'), (strpos(current_url(), ' ')) ? ' icon-list-hover' : ''); ?> -->
+                    <?php echo sprintf('<li><a href="%s"><i class="material-icons%s">insert_drive_file</i></a></li>', base_url('modelofiscal'), (strpos(current_url(), 'modelofiscal')) ? ' icon-list-hover' : ''); ?>
+                    <?php echo sprintf('<li><a href="%s"><i class="material-icons%s">today</i></a></li>', base_url('calendario'), (strpos(current_url(), 'calendario')) ? ' icon-list-hover' : ''); ?>
+                    <?php echo sprintf('<li><a href="%s"><i class="material-icons%s">people</i></a></li>', base_url('clientesproveedores'), (strpos(current_url(), 'clientesproveedores')) ? ' icon-list-hover' : ''); ?>
+                    <?php echo sprintf('<li><a href="%s"><i class="material-icons%s">settings</i></a></li>', base_url('perfil'), (strpos(current_url(), 'perfil')) ? ' icon-list-hover' : ''); ?>
+                    <?php echo sprintf('<li><a href="%s"><i class="material-icons%s">headset_mic</i></a></li>', base_url('soporte'), (strpos(current_url(), 'soporte')) ? ' icon-list-hover' : ''); ?>
+                    <li><a href="<?= base_url('logout'); ?>"><i class="material-icons">exit_to_app</i></a></li>
+                </ul>
+            </ul>
+        </div>
+    <?php endif; ?>
     <!-- Imprimimos el contenido -->
-    <?= isset($main) ? $main : '' ?>
+    <?= isset($main) ? (($this->session->userdata('logged_in')) ? '<div class="container-main">' . $main . '</div>' : $main) : '' ?>
 
     <!-- Validamos que se use app de vue entonces tenemos que ver que app no sea una etiqueta verficiando que su id no sea string si lo es, app es una etiqueta -->
     <script>
-        typeof app.id == 'string' ? null : app.mount('#app');
+        (typeof(app) == 'undefined' || typeof(app.id) == 'string') ? null: app.mount('#app');
     </script>
 
     <script>
         M.AutoInit();
     </script>
 
+
     <script>
-        function verificarExistenciaImagen(url, callback) {
-            var img = new Image();
-            img.onload = function() {
-                callback(true);
+        function cambiarVista(valor = '') {
+            var requestOptions = {
+                method: 'GET',
+                redirect: 'follow'
             };
-            img.onerror = function() {
-                callback(false);
-            };
-            img.src = url;
+
+            fetch("<?php echo base_url('herramientas/cambiarVista/'); ?>" + valor.toString(), requestOptions)
+                .then((response) => response.json())
+                .then((result) => {
+                    // let resultado = result;
+                    // console.log(resultado);
+                    location.reload(true);
+
+                })
+                .catch(error => console.log('error', error));
+
+
         }
 
-        var urlImagen = "";
+        let miImagen = document.getElementById('logoValidar');
+        miImagen.onload = function() {
+            console.log('La imagen se ha cargado correctamente.');
+            // Aquí puedes realizar acciones adicionales después de que la imagen se ha cargado.
+        };
 
-        verificarExistenciaImagen(urlImagen, function(existe) {
-            let logoEmpresaDiv = document.getElementById("logoEmpresa");
-            if (existe) {
-                logoEmpresaDiv.innerHTML = "<img class='logoEmpresa image-center hide-on-med-and-down' src='" + urlImagen + "' alt='Imagen'> ";
-            } else {
-                logoEmpresaDiv.innerHTML = '<h4 class="image-center p-3 hide-on-med-and-down"> <?php echo "hola" ?>  </h4>';
-            }
-        });
+        miImagen.onerror = function() {
+            // console.log('Error al cargar la imagen.');
+             // Crea un nuevo elemento 'h4'
+            var nuevoH4 = document.createElement('h6');
+            nuevoH4.className = 'nombreEmpresa hide-on-med-and-down'; // Agrega las clases necesarias
+
+            // Agrega el texto que deseas dentro del h4
+            nuevoH4.textContent = '<?= $this->session->userdata('datosEmpresa')['short_name'] ?>';
+
+            // Reemplaza la imagen con el nuevo h4
+            miImagen.parentNode.replaceChild(nuevoH4, miImagen);
+            console.log(miImagen);
+        };
     </script>
     <style>
+        .nav-max {
+            height: 40px !important;
+            position: relative;
+        }
+
         /* Styles sidebar */
         .sidebar {
             position: fixed;
@@ -103,7 +153,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
             width: 60px;
             background-color: #333;
             color: #fff;
-            z-index: 2;
+            z-index: 3;
         }
 
         .sidebar ul {
@@ -153,23 +203,19 @@ defined('BASEPATH') or exit('No direct script access allowed');
             justify-content: space-between;
             margin-bottom: 0px !important;
         }
-
-        .nav h4 {
+        .nombreEmpresa{
+            min-width: 200px;
+            text-align: center;
+            font-weight: bold;
+        }
+        .nav h4, .nav h6 {
             color: #444;
         }
 
         .nav p {
             color: #888;
-            margin: 0;
-            padding: 0;
-            margin-top: -15px;
         }
 
-        .nav-price {
-            margin: 0 !important;
-            padding: 5px !important;
-            margin-top: -45px !important;
-        }
 
         .nav-image {
             max-width: 150px;
@@ -203,7 +249,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
         }
 
         .logoEmpresa {
-            max-width: 80px;
+            max-width: 60px;
         }
 
         .logoUsuario {
@@ -217,6 +263,11 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
         .alinearVertical {
             align-items: center;
+        }
+
+        .balance {
+            font-size: 12px;
+            color: #bdbdbd;
         }
     </style>
 </body>
