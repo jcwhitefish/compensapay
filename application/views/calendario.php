c7fd30f8db493afbc0f001cdd137b7f8e18cdebb
Jose Antonio <joserdz052@gmail.com>
2023-10-20 06:47:57 -0600

    ADD all

--- application/views/calendario.php
+++ application/views/calendario.php
@@ -14,13 +14,13 @@
                     <a @click="cambiaModelo(2)" :class="`btn btnMDA ${modeloTabla != 2 ? 'transparent black-text' : 'black'}  waves-effect waves-light`">Mes</a>
                 </div>
                 <div class="input-field col l6">
-                    <input v-model="fecha" class="fechas datepicker" type="text" id="date">
+                    <!-- TODO:La etiqueta no-autoinit te ayuda a avitar que el autoinit lo haga en automatico -->
+                    <input @change="crearCalendario" class="fechas datepicker no-autoinit" type="text" id="date" value="<?= date('d/m/Y'); ?>">
                     <i class="material-icons prefix">arrow_drop_down</i>
                 </div>
 
             </div>
         </div>
-        <div class="col l4">{{fecha}}</div>
         <div class="col l2 center-align">
             <div class="row">
                 <div class="col s12"><a class="btnLeyenda btn-small cyan accent-3">Programadas</a></div>
@@ -35,146 +35,25 @@
 
         </div>
     </div>
-    <!-- Aqui va lo de mes -->
-    <div class="row tablas" v-if="modeloTabla == 2">
+    <div class="row tablas">
         <table>
             <thead>
                 <tr>
-                    <th>Domingo</th>
-                    <th>Lunes</th>
-                    <th>Martes</th>
-                    <th>Miercoles</th>
-                    <th>Jueves</th>
-                    <th>Viernes</th>
-                    <th>Sabado</th>
+                    <template v-for="(day,index) in daysString" :key="index">
+                        <th if="(modeloTabla == 2)||(modeloTabla == 1)||(modeloTabla == 0 && datosDia['diaSemana'] == index)"> {{daysString[index]}}</th>
+                    </template>
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td><span>
-                            <p>1</p>
-                            
-                            <a class="btnLeyenda btn-small red accent-3" style="font-size:11px;">Frontier - A404 - $120,0</a>
-                        </span></td>
-                    <td><span>
-                            <p>2</p>
-                        </span></td>
-                    <td><span>
-                            <p>3</p>
-                        </span></td>
-                </tr>
-                <tr>
-                    <td><span>
-                            <p>4</p>
-                        </span></td>
-                    <td><span>
-                            <p>5</p>
-                        </span></td>
-                    <td><span>
-                            <p>6</p>
-                            <a class="btnLeyenda btn-small green accent-3" style="font-size:11px;">Frontier - A404 - $120,0</a>
-                        </span></td>
-                    <td><span>
-                            <p>7</p>
-                        </span></td>
-                    <td><span>
-                            <p>8</p>
-                        </span></td>
-                    <td><span>
-                            <p>9</p>
-                        </span></td>
-                    <td><span>
-                            <p>10</p>
-                        </span></td>
-                </tr>
-                <tr>
-                    <td><span>
-                            <p>11</p>
-                        </span></td>
-                    <td><span>
-                            <p>12</p>
-                            
-                            <a class="btnLeyenda btn-small cyan accent-3" style="font-size:11px;">Frontier - A404 - $120,0</a>
-                        </span></td>
-                    <td><span>
-                            <p>13</p>
-                        </span></td>
-                    <td><span>
-                            <p>14</p>
-
-                            <a class="btnLeyenda btn-small cyan accent-3" style="font-size:11px;">Frontier - A404 - $120,0</a>
-                        </span></td>
-                    <td><span>
-                            <p>15</p>
-                        </span></td>
-                    <td><span>
-                            <p>16</p>
-                        </span></td>
-                    <td>
-                        <span>
-                            <p>17</p>
-                            <a class="btnLeyenda btn-small red accent-3" style="font-size:11px;">Frontier - A404 - $120,0</a>
-                        </span>
-
-                    </td>
-                </tr>
-                <tr>
-                    <td><span>
-                            <p>18</p>
-                            <a class="btnLeyenda btn-small cyan accent-3" style="font-size:11px;">Frontier - A404 - $120,0</a>
-                        </span></td>
-                    <td><span>
-                            <p>19</p>
-
-                        </span></td>
-                    <td><span>
-                            <p>20</p>
-                        </span></td>
-                    <td><span>
-                            <p>21</p>
-                        </span></td>
-                    <td><span>
-                            <p>22</p>
-                            
-                            <a class="btnLeyenda btn-small green accent-3" style="font-size:11px;">Frontier - A404 - $120,0</a>
-                        </span></td>
-                    <td><span>
-                            <p>23</p>
-                        </span></td>
-                    <td><span>
-                            <p>24</p>
-                        </span></td>
-                </tr>
-                <tr>
-                    <td><span>
-                            <a class="btnLeyenda btn-small green accent-3" style="font-size:11px;">Frontier - A404 - $120,0</a>
-                            <p>25</p>
-                        </span></td>
-                    <td><span>
-                            <p>26</p>
-                        </span></td>
-                    <td><span>
-                            <p>27</p>
-                        </span></td>
-                    <td><span>
-                            <p>28</p>
-                        </span></td>
-                    <td><span>
-                            <p>29</p>
-                        </span></td>
-                    <td><span>
-                            <p>30</p>
-                            
-                            <a class="btnLeyenda btn-small cyan accent-3" style="font-size:11px;">Frontier - A404 - $120,0</a>
-                            
-                            <a class="btnLeyenda btn-small green accent-3" style="font-size:11px;">Frontier - A404 - $120,0</a>
-                        </span></td>
-                    <!-- Puedes añadir más días si es necesario -->
-                </tr>
+                <template v-for="(semana,indiceSemana) in calendario" :key="indiceSemana">
+                    <tr v-if="(modeloTabla == 2)||(modeloTabla == 1 && datosDia['semana'] == indiceSemana)|| (modeloTabla == 0)">
+                        <template v-for="(celda,indiceCelda) in 7" :key="indiceCelda">
+                            <td v-if="(modeloTabla == 2)||(modeloTabla == 1)||(modeloTabla == 0 && datosDia['semana'] == indiceSemana && datosDia['diaSemana'] == indiceCelda)">
+                                <p class="numeroDia">{{`${typeof semana[indiceCelda] == 'object' ? semana[indiceCelda]['numero'] : ''}`}}</p>
+                            </td>
+                        </template>
+                    </tr>
+                </template>
             </tbody>
         </table>
     </div>
@@ -192,32 +71,88 @@
     const app = createApp({
         setup() {
             const modeloTabla = ref(2);
-            const fecha = ref(null);
+            const fecha = ref('');
             const cambiaModelo = (modelo) => {
                 modeloTabla.value = modelo;
             }
+            const datosDia = ref([]);
+            const calendario = ref([]);
+            const daysString = ref(['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado']);
             const formatearDate = () => {
-                var options = {
+                let options = {
                     format: 'dd/mm/yyyy', // Formato de fecha
+                    setDefaultDate: true, // Inicializar con la fecha actual
                     autoClose: true, // Cerrar automáticamente después de seleccionar la fecha
-                    // Puedes agregar más opciones según tus necesidades
+
                 };
-                nextTick(() => {
-                    // M.Datepicker.init(document.getElementById('date'),options);
-                    M.AutoInit();
-                });
+
+                let datepicker = document.getElementById('date');
+
+                let instance = M.Datepicker.init(datepicker, options);
+
+
+            }
+
+
+            const crearCalendario = () => {
+                let datepicker = document.getElementById('date');
+                fecha.value = datepicker.value;
+                //obtenermos el dia mes y año de la variable fecha y los metemos en distintas variables separando el string por /
+                let [day, month, year] = fecha.value.split('/');
+                //obtenemos en que dia de la semana cae el primer dia del mes
+                let firstDay = new Date(year, month - 1, 1).getDay();
+
+                //console.log(firstDay);
+                //lo convertimos a letras
+
+                let firstDayString = daysString.value[firstDay];
+                //console.log(firstDayString);
+
+                //obtenemos el ultimo dia del mes
+                let lastDay = new Date(year, month, 0).getDate();
+                //console.log(lastDay);
+                //obtenemso las variables x y y
+                let x = firstDay; //x inicia en el dia de la semana que cae el primer dia del mes
+                let y = 0;
+                //creamos z que es el dia que se va a imprimir en el calendario
+                let z = 1;
+                //creamos el calendario
+                calendario.value[y] = [];
+                while (lastDay >= z) {
+                    calendario.value[y][x] = {
+                        'numero': z
+                    };
+                    if (z == day) {
+                        datosDia.value['diaSemana'] = x;
+                        datosDia.value['semana'] = y;
+                        datosDia.value['diaNumero'] = z;
+                    }
+                    if (x == 6) {
+                        x = 0;
+                        y++;
+                        calendario.value[y] = [];
+                    } else {
+                        x++;
+                    }
+                    z++;
+                }
 
             }
             onMounted(
                 () => {
                     formatearDate();
+                    crearCalendario();
                 }
             )
             // Retornar el estado y métodos
             return {
                 modeloTabla,
                 cambiaModelo,
-                fecha
+                fecha,
+                daysString,
+                calendario,
+                crearCalendario,
+                datosDia
             };
         },
     });
@@ -225,6 +160,15 @@
 
 
 <style>
+    th {
+        background-color: #fafafa !important;
+    }
+
+    /* Seleccionamos a los elementos th que estan dentro de tables y les ponemos color red */
+    .tablas th {
+        background-color: #17a2b8 !important;
+    }
+
     .btnLeyenda {
         border-radius: 25px !important;
         width: 130px;
@@ -265,7 +209,7 @@
 
     /* Estos estilos son del datapicker */
     .datepicker-date-display {
-        background-color: #e0e51d !important;
+        background-color: #17a2b8 !important;
 
     }
 
@@ -273,26 +217,26 @@
     .datepicker-clear,
     .datepicker-today,
     .datepicker-done {
-        color: #e0e51d;
+        color: #17a2b8;
     }
 
     .datepicker-table td.is-today {
-        color: #e0e51d;
+        color: #17a2b8;
     }
 
     .datepicker-table td.is-selected {
-        background-color: #e0e51d;
+        background-color: #17a2b8;
         color: #fff;
     }
 
     .dropdown-content li>a,
     .dropdown-content li>span {
-        color: #e0e51d;
+        color: #17a2b8;
     }
 
     button:focus {
         outline: none;
-        background-color: #e0e51d;
+        background-color: #17a2b8;
     }
 
     .tablas thead {
@@ -301,30 +245,16 @@
 
     .tablas td {
         border: 1px solid #eeeeee;
-        height: 3cm;
-        width: 3cm;
-    }
-
-    .tablas td span {
-
-        flex-direction: column;
-        font-weight: bold;
-        display: flex;
-        align-items: center
-    }
-
-    .tablas td span p {
-        position: relative;
-        width: 100%;
-        text-align: right;
+        min-height: 3cm;
     }
 
     /* quitar el hover */
-    tbody tr:hover{
+    tbody tr:hover {
         background-color: white !important;
     }
-</style>
 
-<!-- cadena_validar = '{"Usuario":"DemoUser","Llave":"Pasrd"}';
-$validar_acceso=$this->Interaccionbd->ValidarAcceso($cadena_validar);
-echo $validar_acceso; -->
+    .numeroDia {
+        text-align: end;
+        font-weight: bold;
+    }
+</style>
