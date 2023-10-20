df31de0df47fa1257b6d7236b935b18f5a1a0886
AbdielMg007 <abdiel.mugo@gmail.com>
2023-10-20 13:55:48 -0600

    FIX Notes

--- application/helpers/factura_helper.php
+++ application/helpers/factura_helper.php
@@ -37,78 +37,10 @@ if (!function_exists('procesar_xml')) {
 if (!function_exists('procesar_factura_relacional')) {
     function procesar_factura_relacional($xml) {
         $comprobante = $xml->getElementsByTagName('Comprobante')->item(0);
-		// $version = $comprobante->getAttribute('Version');
-		// $serie = $comprobante->getAttribute('Serie');
-		// $folio = $comprobante->getAttribute('Folio');
-		// $fecha = $comprobante->getAttribute('Fecha');
-		// $sello = $comprobante->getAttribute('Sello');
-		// $subtotal = $comprobante->getAttribute('SubTotal');
-		// $moneda = $comprobante->getAttribute('Moneda');
-		// $tipoCambio = $comprobante->getAttribute('TipoCambio');
-		// $total = $comprobante->getAttribute('Total');
-		// $tipoDeComprobante = $comprobante->getAttribute('TipoDeComprobante');
-		// $metodoPago = $comprobante->getAttribute('MetodoPago');
-		// $formaPago = $comprobante->getAttribute('FormaPago');
-		// $lugarExpedicion = $comprobante->getAttribute('LugarExpedicion');
-		// $cfdiRelacionados = $comprobante->getAttribute('CfdiRelacionados');
-
-		// // Acceder a datos del Emisor
 		$emisor = $xml->getElementsByTagName('Emisor')->item(0);
-		// $rfcEmisor = $emisor->getAttribute('Rfc');
-		// $nombreEmisor = $emisor->getAttribute('Nombre');
-		// $regimenFiscalEmisor = $emisor->getAttribute('RegimenFiscal');
-
-		// // Acceder a datos del Receptor
 		$receptor = $xml->getElementsByTagName('Receptor')->item(0);
-		// $rfcReceptor = $receptor->getAttribute('Rfc');
-		// $nombreReceptor = $receptor->getAttribute('Nombre');
-		// $domicilioFiscalReceptor = $receptor->getAttribute('DomicilioFiscalReceptor');
-		// $regimenFiscalReceptor = $receptor->getAttribute('RegimenFiscalReceptor');
-		// $usoCFDI = $receptor->getAttribute('UsoCFDI');
-
-		// // Acceder a datos de Conceptos (puedes tener múltiples conceptos)
-		// $conceptos = $xml->getElementsByTagName('Concepto');cfdi:cfdi:
-		// foreach ($conceptos as $concepto) {
-		// 	$claveProdServ = $concepto->getAttribute('ClaveProdServ');
-		// 	$cantidad = $concepto->getAttribute('Cantidad');
-		// 	$claveUnidad = $concepto->getAttribute('ClaveUnidad');
-		// 	$unidad = $concepto->getAttribute('Unidad');
-		// 	$descripcion = $concepto->getAttribute('Descripcion');
-		// 	$valorUnitario = $concepto->getAttribute('ValorUnitario');
-		// 	$importe = $concepto->getAttribute('Importe');
-		// 	$objetoImp = $concepto->getAttribute('ObjetoImp');
-			
-		// 	// Acceder a datos de Impuestos dentro de Concepto
-		// 	$impuestos = $concepto->getElementsByTagName('Impuestos')->item(0);
-		// 	$traslados = $impuestos->getElementsByTagName('Traslados')->item(0);
-		// 	$traslado = $traslados->getElementsByTagName('Traslado')->item(0);
-		// 	$base = $traslado->getAttribute('Base');
-		// 	$importeImpuesto = $traslado->getAttribute('Importe');
-		// 	$impuesto = $traslado->getAttribute('Impuesto');
-		// 	$tasaOCuota = $traslado->getAttribute('TasaOCuota');
-		// 	$tipoFactor = $traslado->getAttribute('TipoFactor');
-		// }
-
-		// // Acceder a datos de Impuestos Globales
 		$impuestosGlobales = $xml->getElementsByTagName('Impuestos')->item(0);
-		// $totalImpuestosTrasladados = $impuestosGlobales->getAttribute('TotalImpuestosTrasladados');
-		// $trasladosGlobales = $impuestosGlobales->getElementsByTagName('Traslados')->item(0);
-		// $trasladoGlobal = $trasladosGlobales->getElementsByTagName('Traslado')->item(0);
-		// $baseGlobal = $trasladoGlobal->getAttribute('Base');
-		// $importeImpuestoGlobal = $trasladoGlobal->getAttribute('Importe');
-		// $impuestoGlobal = $trasladoGlobal->getAttribute('Impuesto');
-		// $tasaOCuotaGlobal = $trasladoGlobal->getAttribute('TasaOCuota');
-		// $tipoFactorGlobal = $trasladoGlobal->getAttribute('TipoFactor');
-
-		// // Acceder a datos del Timbre Fiscal Digital
 		$timbreFiscalDigital = $xml->getElementsByTagName('TimbreFiscalDigital')->item(0);
-		// $versionTimbre = $timbreFiscalDigital->getAttribute('Version');
-		// $uuid = $timbreFiscalDigital->getAttribute('UUID');
-		// $fechaTimbrado = $timbreFiscalDigital->getAttribute('FechaTimbrado');
-		// $rfcProvCertif = $timbreFiscalDigital->getAttribute('RfcProvCertif');
-		// $selloCFD = $timbreFiscalDigital->getAttribute('SelloCFD');
-		// $noCertificadoSAT = $timbreFiscalDigital->getAttribute('NoCertificadoSAT');
-		// $selloSAT = $timbreFiscalDigital->getAttribute('SelloSAT');
 
         $factura = array(
 			"fecha" => $comprobante->getAttribute('Fecha'),
@@ -119,5 +51,27 @@ if (!function_exists('procesar_factura_relacional')) {
 		);
 
         return $factura;
+    }
+}
+
+if (!function_exists('procesar_nota_relacional')) {
+    function procesar_nota_relacional($xml, $id_invoice) {
+
+		$comprobante = $xml->getElementsByTagName('Comprobante')->item(0);
+		$cfdiRelacionados = $xml->getElementsByTagName('CfdiRelacionados')->item(0);
+		//$tipoRelacion = $cfdiRelacionados->getAttribute('TipoRelacion'); importante pasa saber si es nota
+		$uuidRelacionado = $cfdiRelacionados->getElementsByTagName('CfdiRelacionado')->item(0)->getAttribute('UUID');
+		$impuestosGlobales = $xml->getElementsByTagName('Impuestos')->item(0);
+
+        $nota = array(
+			"id_invoice" => $id_invoice,
+			"note_number" => str_pad(rand(1, 99999999), 8, '0', STR_PAD_LEFT),
+			"total" => $comprobante->getAttribute('Total'),
+			"xml_document" => $xml->saveXML(),
+			"created_at" => $comprobante->getAttribute('Fecha'),
+			"uuid" => 	$uuidRelacionado = $cfdiRelacionados->getElementsByTagName('CfdiRelacionado')->item(0)->getAttribute('UUID'),			
+		);
+
+        return $nota;
     }
 }
