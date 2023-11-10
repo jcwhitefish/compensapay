d00ef1d3e597ae40c4a2e6ae8092057c749f302a
Confiur <fernando_alarcon_r@hotmail.com>
2023-11-10 10:20:55 -0600

    Summary
    Firma en contrato con imagen

--- application/models/Proveedor_model.php
+++ application/models/Proveedor_model.php
@@ -18,16 +18,24 @@ class Proveedor_model extends CI_Model{
 	 */
 	public function registrarProveedor (array $args, ?string $env = 'SANDBOX'){
 		
-
-		$query = "INSERT INTO compensatest_base.rec_supplier (id_com, nationality, date_const, num_rpc, e_firma, e_mail, website, obj_social, desc_operation, transact_month, ammount, person_incharge, person_name, person_curp, person_rfc, person_id, person_adress, person_email, person_phone, natural_person_benef, benef_legal_entity, license_services, supervisor_name, license_type, license_date, been_audited, anticorruption, data_protection, vul_activity, regist_sat, up_to_date) 
-            VALUES ({$args['companie']}, '{$args['nationality']}', '{$args['dateConst']}','{$args['folio']}', '{$args['efirma']}', '{$args['emailForm']}', '{$args['web']}', '{$args['socialobj']}', '{$args['descOperation']}', '{$args['transactMonth']}', '{$args['amount']}', '{$args['charge']}', '{$args['nameForm2']}', '{$args['curp']}', '{$args['rfcForm2']}', '{$args['idNumber']}', '{$args['domForm2']}', '{$args['emailForm2']}', '{$args['phoneForm2']}', {$args['fisica']}, {$args['moral']}, {$args['license']}, '{$args['supervisor']}', '{$args['typeLicense']}', '{$args['dateAward']}', {$args['audited']}, {$args['anticorruption']}, {$args['dataProtection']}, {$args['vulnerable']}, '{$args['servTrib']}', '{$args['obligations']}')";
-		if ($result = $this->db->query($query)){
-			$id = $this->db->insert_id();
-			return ['id'=>$id];
+		$queryupd = "UPDATE compensapay.companies set legal_name ='{$args['bussinesName']}', short_name = '{$args['nameComercial']}', rfc = '{$args['rfc']}', address = '{$args['dom']}', telephone ='{$args['phoneForm']}', account_clabe = '{$args['clabe']}' where id = {$args['companie']} ";
+		if ($result = $this->db->query($queryupd)){
 			
+			$query = "INSERT INTO compensapay.rec_supplier (id_com, nationality, date_const, num_rpc, e_firma, e_mail, website, obj_social, desc_operation, transact_month, ammount, person_incharge, person_name, person_curp, person_rfc, person_id, person_adress, person_email, person_phone, natural_person_benef, benef_legal_entity, license_services, supervisor_name, license_type, license_date, been_audited, anticorruption, data_protection, vul_activity, regist_sat, up_to_date) 
+            VALUES ({$args['companie']}, '{$args['nationality']}', '{$args['dateConst']}','{$args['folio']}', '{$args['efirma']}', '{$args['emailForm']}', '{$args['web']}', '{$args['socialobj']}', '{$args['descOperation']}', '{$args['transactMonth']}', '{$args['amount']}', '{$args['charge']}', '{$args['nameForm2']}', '{$args['curp']}', '{$args['rfcForm2']}', '{$args['idNumber']}', '{$args['domForm2']}', '{$args['emailForm2']}', '{$args['phoneForm2']}', {$args['fisica']}, {$args['moral']}, {$args['license']}, '{$args['supervisor']}', '{$args['typeLicense']}', '{$args['dateAward']}', {$args['audited']}, {$args['anticorruption']}, {$args['dataProtection']}, {$args['vulnerable']}, '{$args['servTrib']}', '{$args['obligations']}')";
+			if ($result = $this->db->query($query)){
+				$id = $this->db->insert_id();
+				return ['id'=>$id];
+				
+			}else{
+				return ($result->error());
+			}
+
 		}else{
 			return ($result->error());
 		}
+
+		
 	}
 
 	public function createPDF (array $args, ?string $env = 'SANDBOX'){
@@ -157,6 +165,9 @@ class Proveedor_model extends CI_Model{
 										nuestra página de internet: <u>cuenca.com</u></p>
 
 									<p>La información contenida en este documento es privada y confidencial.</p>
+									<div style="text-align: right; width:80%">
+										<img width="150" height="80" src="data:image/png;base64,'. $args['firma'] .'">
+									</div>
 									<p>Firma del representante legal: _________________________________________________________</p>
 									<p>A. Información general</p>
 									<table class="borders">
