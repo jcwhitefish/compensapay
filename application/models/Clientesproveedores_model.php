7749886cebbc1643823b69154979526499128ade
Juan Carlos Carreno <jkarreno@gmail.com>
2023-11-21 11:55:31 -0600

    ADD se agrego la vista cliente/proveedor

--- application/models/Clientesproveedores_model.php
+++ application/models/Clientesproveedores_model.php
@@ -6,17 +6,45 @@ class Clientesproveedores_model extends CI_Model {
     }
 
     public function clientes(){
-        $queryc = "SELECT * FROM clientprovider GROUP BY client_id";
-        $clientes = $this->db->query($query);
 
-        if($clientes->num_rows > 0)
-        {
-            return $clientes;
+        $idCompanie = $this->session->userdata('datosEmpresa')['id'];
+
+        $queryc = "SELECT * FROM clientprovider AS c 
+                    INNER JOIN companies AS com ON c.client_id=com.id 
+                    INNER JOIN cat_zipcode AS cp ON com.id_postal_code=cp.zip_id 
+                    WHERE provider_id='".$idCompanie."' 
+                    ORDER BY com.legal_name;";
+
+        $queryp = "SELECT * FROM clientprovider AS c
+                    INNER JOIN companies AS com On c.provider_id=com.id
+                    INNER JOIN cat_zipcode AS cp on com.id_postal_code=cp.zip_id 
+                    WHERE client_id='".$idCompanie."' 
+                    ORDER BY com.legal_name;";
+
+        if ($result = $this->db->query($queryc)) {
+			if ($result->num_rows() > 0){
+                $resultc = $result->result_array();
+            }
+            else {
+                $resultc = '';
+            }
+		}
+
+        if($resul2 = $this->db->query($queryp)) {
+            if($resul2->num_rows() >0){
+                $resultp = $resul2->result_array();
+            }
+            else{
+                $resultp = '';
+            }
         }
-        else
-        {
-            return FALSE;
-        }
+
+        $rescp = array (
+            "clientes" => $resultc, 
+            "proveedores" => $resultp
+        );
+
+		return $rescp;
     }
 
     //public function get_cp(int $id) {
--- application/views/clientesproveedores/clientesprovedores.php
+++ application/views/clientesproveedores/clientesprovedores.php
@@ -1,7 +1,3 @@
-<?php
-var_dump($clipro);
-?>
-
 <div class="p-5" id="app">
     <div class="row">
         <div class="col l12 right-align p-5">
@@ -30,248 +26,69 @@ var_dump($clipro);
                 <table v-if="selectedButton === 'Clientes'" class="visible-table striped">
                     <thead>
                         <tr>
-                            <th >RFC</th>
-                            <th >Alias</th>
-                            <th >Razón Social</th>
-                            <th >Dirección</th>
-                            <th >Contacto</th>
-                            <th >Teléfono</th>
-                            <th >Correo electrónico</th>
-                            <th >Fecha de alta</th>
+                            <th>Cliente</th>
+                            <th>Alias</th>
+                            <th>RFC</th>
+                            <th>Dirección</th>
+                            <th>Código postal</th>
+                            <th>Teléfono</th>
+                            <th>Fecha de alta</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td >ABC123456</td>
-                            <td >Empresa1</td>
-                            <td >Nombre Empresa 1</td>
-                            <td >Dirección Empresa 1</td>
-                            <td >Contacto Empresa 1</td>
-                            <td >123-456-7890</td>
-                            <td >correo1@example.com</td>
-                            <td >2023-01-15</td>
-                        </tr>
-                        <tr>
-                            <td >DEF789012</td>
-                            <td >Empresa2</td>
-                            <td >Nombre Empresa 2</td>
-                            <td >Dirección Empresa 2</td>
-                            <td >Contacto Empresa 2</td>
-                            <td >987-654-3210</td>
-                            <td >correo2@example.com</td>
-                            <td >2022-08-20</td>
-                        </tr>
-                        <tr>
-                            <td >ABC123456</td>
-                            <td >Empresa3</td>
-                            <td >Nombre Empresa 3</td>
-                            <td >Dirección Empresa 3</td>
-                            <td >Contacto Empresa 3</td>
-                            <td >123-456-7890</td>
-                            <td >correo3@example.com</td>
-                            <td >2022-09-15</td>
-                        </tr>
-
-                        <tr>
-                            <td >XYZ789012</td>
-                            <td >Empresa4</td>
-                            <td >Nombre Empresa 4</td>
-                            <td >Dirección Empresa 4</td>
-                            <td >Contacto Empresa 4</td>
-                            <td >345-678-9012</td>
-                            <td >correo4@example.com</td>
-                            <td >2022-10-05</td>
-                        </tr>
-
-                        <tr>
-                            <td >GHI345678</td>
-                            <td >Empresa5</td>
-                            <td >Nombre Empresa 5</td>
-                            <td >Dirección Empresa 5</td>
-                            <td >Contacto Empresa 5</td>
-                            <td >567-890-1234</td>
-                            <td >correo5@example.com</td>
-                            <td >2022-11-12</td>
-                        </tr>
-
-                        <tr>
-                            <td >JKL678901</td>
-                            <td >Empresa6</td>
-                            <td >Nombre Empresa 6</td>
-                            <td >Dirección Empresa 6</td>
-                            <td >Contacto Empresa 6</td>
-                            <td >678-901-2345</td>
-                            <td >correo6@example.com</td>
-                            <td >2022-12-20</td>
-                        </tr>
-
-                        <tr>
-                            <td >MNO901234</td>
-                            <td >Empresa7</td>
-                            <td >Nombre Empresa 7</td>
-                            <td >Dirección Empresa 7</td>
-                            <td >Contacto Empresa 7</td>
-                            <td >789-012-3456</td>
-                            <td >correo7@example.com</td>
-                            <td >2023-01-25</td>
-                        </tr>
-
-                        <tr>
-                            <td >PQR567890</td>
-                            <td >Empresa8</td>
-                            <td >Nombre Empresa 8</td>
-                            <td >Dirección Empresa 8</td>
-                            <td >Contacto Empresa 8</td>
-                            <td >890-123-4567</td>
-                            <td >correo8@example.com</td>
-                            <td >2023-02-18</td>
-                        </tr>
-
-                        <tr>
-                            <td >STU678901</td>
-                            <td >Empresa9</td>
-                            <td >Nombre Empresa 9</td>
-                            <td >Dirección Empresa 9</td>
-                            <td >Contacto Empresa 9</td>
-                            <td >901-234-5678</td>
-                            <td >correo9@example.com</td>
-                            <td >2023-03-10</td>
-                        </tr>
-
-                        <tr>
-                            <td >VWX789012</td>
-                            <td >Empresa10</td>
-                            <td >Nombre Empresa 10</td>
-                            <td >Dirección Empresa 10</td>
-                            <td >Contacto Empresa 10</td>
-                            <td >012-345-6789</td>
-                            <td >correo10@example.com</td>
-                            <td >2023-04-05</td>
-                        </tr>
+                        <?php
+                        if(is_array($clipro["clientes"]))
+                        {
+                            foreach ($clipro["clientes"] as $value)
+                            {
+                                echo '<tr>
+                                        <td>'.$value["legal_name"].'</td>
+                                        <td>'.$value["short_name"].'</td>
+                                        <td>'.$value['rfc'].'</td>
+                                        <td>'.$value["address"].'</td>
+                                        <td>'.$value["zip_code"].'</td>
+                                        <td>'.$value["telephone"].'</td>
+                                        <td>'.date('d-m-Y', $value["created_at"]).'</td>
+                                    </tr>';
+                            }
+                        }
+                        
+                        ?>
                     </tbody>
                 </table>
-                <table v-if="selectedButton === 'Proveedores'" class="visible-table striped">       
-                <thead>
+                <div style="overflow-x: auto;">
+                <table v-if="selectedButton === 'Proveedores'" class="visible-table striped">
+                    <thead>
                         <tr>
-                            <th >RFC</th>
-                            <th >Alias</th>
-                            <th >Razón Social</th>
-                            <th >Dirección</th>
-                            <th >Contacto</th>
-                            <th >Teléfono</th>
-                            <th >Correo electrónico</th>
-                            <th >Fecha de alta</th>
+                            <th>Proveedor</th>
+                            <th>Alias</th>
+                            <th>RFC</th>
+                            <th>Dirección</th>
+                            <th>Código postal</th>
+                            <th>Teléfono</th>
+                            <th>Fecha de alta</th>
                         </tr>
                     </thead>
-
                     <tbody>
-                        <tr>
-                            <td >ABC123456</td>
-                            <td >Empresa1</td>
-                            <td >Nombre Empresa 1</td>
-                            <td >Dirección Empresa 1</td>
-                            <td >Contacto Empresa 1</td>
-                            <td >123-456-7890</td>
-                            <td >correo1@example.com</td>
-                            <td >2023-01-15</td>
-                        </tr>
-                        <tr>
-                            <td >DEF789012</td>
-                            <td >Empresa2</td>
-                            <td >Nombre Empresa 2</td>
-                            <td >Dirección Empresa 2</td>
-                            <td >Contacto Empresa 2</td>
-                            <td >987-654-3210</td>
-                            <td >correo2@example.com</td>
-                            <td >2022-08-20</td>
-                        </tr>
-                        <tr>
-                            <td >ABC123456</td>
-                            <td >Empresa3</td>
-                            <td >Nombre Empresa 3</td>
-                            <td >Dirección Empresa 3</td>
-                            <td >Contacto Empresa 3</td>
-                            <td >123-456-7890</td>
-                            <td >correo3@example.com</td>
-                            <td >2022-09-15</td>
-                        </tr>
-
-                        <tr>
-                            <td >XYZ789012</td>
-                            <td >Empresa4</td>
-                            <td >Nombre Empresa 4</td>
-                            <td >Dirección Empresa 4</td>
-                            <td >Contacto Empresa 4</td>
-                            <td >345-678-9012</td>
-                            <td >correo4@example.com</td>
-                            <td >2022-10-05</td>
-                        </tr>
-
-                        <tr>
-                            <td >GHI345678</td>
-                            <td >Empresa5</td>
-                            <td >Nombre Empresa 5</td>
-                            <td >Dirección Empresa 5</td>
-                            <td >Contacto Empresa 5</td>
-                            <td >567-890-1234</td>
-                            <td >correo5@example.com</td>
-                            <td >2022-11-12</td>
-                        </tr>
-
-                        <tr>
-                            <td >JKL678901</td>
-                            <td >Empresa6</td>
-                            <td >Nombre Empresa 6</td>
-                            <td >Dirección Empresa 6</td>
-                            <td >Contacto Empresa 6</td>
-                            <td >678-901-2345</td>
-                            <td >correo6@example.com</td>
-                            <td >2022-12-20</td>
-                        </tr>
-
-                        <tr>
-                            <td >MNO901234</td>
-                            <td >Empresa7</td>
-                            <td >Nombre Empresa 7</td>
-                            <td >Dirección Empresa 7</td>
-                            <td >Contacto Empresa 7</td>
-                            <td >789-012-3456</td>
-                            <td >correo7@example.com</td>
-                            <td >2023-01-25</td>
-                        </tr>
-
-                        <tr>
-                            <td >PQR567890</td>
-                            <td >Empresa8</td>
-                            <td >Nombre Empresa 8</td>
-                            <td >Dirección Empresa 8</td>
-                            <td >Contacto Empresa 8</td>
-                            <td >890-123-4567</td>
-                            <td >correo8@example.com</td>
-                            <td >2023-02-18</td>
-                        </tr>
-
-                        <tr>
-                            <td >STU678901</td>
-                            <td >Empresa9</td>
-                            <td >Nombre Empresa 9</td>
-                            <td >Dirección Empresa 9</td>
-                            <td >Contacto Empresa 9</td>
-                            <td >901-234-5678</td>
-                            <td >correo9@example.com</td>
-                            <td >2023-03-10</td>
-                        </tr>
-
-                        <tr>
-                            <td >VWX789012</td>
-                            <td >Empresa10</td>
-                            <td >Nombre Empresa 10</td>
-                            <td >Dirección Empresa 10</td>
-                            <td >Contacto Empresa 10</td>
-                            <td >012-345-6789</td>
-                            <td >correo10@example.com</td>
-                            <td >2023-04-05</td>
-                        </tr>
+                        <?php
+                        if(is_array($clipro["proveedores"]))
+                        {
+                            foreach ($clipro["proveedores"] as $value)
+                            {
+                                //print_r($value);
+                                echo '<tr>
+                                        <td>'.$value["legal_name"].'</td>
+                                        <td>'.$value["short_name"].'</td>
+                                        <td>'.$value['rfc'].'</td>
+                                        <td>'.$value["address"].'</td>
+                                        <td>'.$value["zip_code"].'</td>
+                                        <td>'.$value["telephone"].'</td>
+                                        <td>'.date('d-m-Y', $value["created_at"]).'</td>
+                                    </tr>';
+                            }
+                        }
+                        
+                        ?>
                     </tbody>
                 </table>
             </div>
@@ -292,35 +109,22 @@ var_dump($clipro);
     }
 
 </style>
-
 <script>
     const app = Vue.createApp({
         setup() {
-            const invoiceUploadName = Vue.ref('');
+            
             const selectedButton = Vue.ref('Clientes');
 
-            const checkFormatInvoice = (event) => {
-                const fileInput = event.target;
-                if (fileInput.files.length > 0) {
-                    invoiceUploadName.value = fileInput.files[0].name;
-                } else {
-                    invoiceUploadName.value = '';
-                }
-            };
-
             const selectButton = (buttonName) => {
-                if (selectedButton.value == buttonName) {
-                    selectedButton.value = null;
-                } else {
+                if (selectedButton.value != buttonName) {
                     selectedButton.value = buttonName;
-                }
+                } 
             };
 
             return {
-                invoiceUploadName,
+                
                 selectedButton,
-                checkFormatInvoice,
-                selectButton,
+                selectButton
             };
         }
     });
