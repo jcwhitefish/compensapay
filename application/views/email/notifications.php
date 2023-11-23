b58d5979d1f87e4f3aaa3ea8309e4efe4e9cc277
DRAGKOZ <skdkuriel@hotmail.com>
2023-11-23 16:55:33 -0600

    Add notifications mail view

---
+++ application/views/email/notifications.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Notificaciones</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+<body style="margin:0; padding:0;" class="grey darken-4">
+<table class=" grey darken-4 white-text" style="border-collapse: unset">
+	<tr>
+		<td class="black center-align">
+			<image class="center-align responsive-img" style="margin: auto; max-height: 225px" id="imgHeader" src="https://compensapay.xyz/assets/images/SolveCapital_Branding-24.png" alt="SOLVE"></image>
+		</td>
+	</tr>
+	<tr>
+		<td><h5>Estimado {}, de la empresa {}.</h5></td>
+	</tr>
+	<tr>
+		<td>Te informamos que:</td>
+	</tr>
+	<tr>
+		<td>actividad</td>
+	</tr>
+	<tr>
+		<td>Para más detalles ingresa con tu cuenta a  nuestra plataforma en <a href="https://compensapay.xyz/">{}</a></td>
+	</tr>
+	<tr>
+		<td>Cualquier duda o comentario, no dudes en contactarnos en el módulo de atención a clientes: Liga o contáctanos por ayuda@solve.com.mx.</td>
+	</tr>
+	<tr>
+		<td>
+			Atentamente,<br>Equipo Solve
+		</td>
+	</tr>
+</table>
+</body>
+</html>
