fcaa82c2586b1c2ff7ffbfee8adf851deb7ef6f1
Juan Carlos Carreno <jkarreno@gmail.com>
2024-01-11 10:04:15 -0600

    FIX reportes

--- application/models/Reportes_model.php
+++ application/models/Reportes_model.php
@@ -10,13 +10,13 @@ class Reportes_model extends CI_Model {
         $idCompanie = $this->session->userdata('datosEmpresa')['id'];
 
         $ResRep  = "SELECT cp.provider_id, cp.client_id, c.id, c.legal_name, c.short_name, COUNT(o.id) AS operaciones, 
-                    (SELECT COUNT(Id) FROM operations WHERE id_client = c.id OR id_provider = c.id AND status = 3) AS realizadas, 
-                    (SELECT COUNT(Id) FROM operations WHERE id_client = c.id OR id_provider = c.id AND status = 2) AS canceladas, 
-                    (SELECT COUNT(Id) FROM operations WHERE id_client = c.id OR id_provider = c.id AND status = 0) AS pendientes, 
-                    (SELECT COUNT(Id) FROM operations WHERE id_client = c.id OR id_provider = c.id AND status = 1) AS autorizadas, 
-                    (SELECT created_at FROM operations WHERE id_client = c.id OR id_provider = c.id ORDER BY created_at LIMIT 1) AS ultimaoperacion, 
-                    (SELECT SUM(entry_money) FROM operations WHERE id_client = c.id OR id_provider = cp.provider_id ORDER BY created_at LIMIT 1) AS ingresos, 
-                    (SELECT SUM(exit_money) FROM operations WHERE id_client = c.id OR id_provider = cp.provider_id ORDER BY created_at LIMIT 1) AS egresos 
+                    (SELECT COUNT(Id) FROM operations WHERE id_client = ".$idCompanie." OR id_provider = ".$idCompanie." AND status = 3) AS realizadas, 
+                    (SELECT COUNT(Id) FROM operations WHERE id_client = ".$idCompanie." OR id_provider = ".$idCompanie." AND status = 2) AS canceladas, 
+                    (SELECT COUNT(Id) FROM operations WHERE id_client = ".$idCompanie." OR id_provider = ".$idCompanie." AND status = 0) AS pendientes, 
+                    (SELECT COUNT(Id) FROM operations WHERE id_client = ".$idCompanie." OR id_provider = ".$idCompanie." AND status = 1) AS autorizadas, 
+                    (SELECT created_at FROM operations WHERE id_client = ".$idCompanie." OR id_provider = ".$idCompanie." ORDER BY created_at LIMIT 1) AS ultimaoperacion, 
+                    (SELECT SUM(entry_money) FROM operations WHERE id_client = ".$idCompanie." OR id_provider = cp.provider_id ORDER BY created_at LIMIT 1) AS ingresos, 
+                    (SELECT SUM(exit_money) FROM operations WHERE id_client = ".$idCompanie." OR id_provider = cp.provider_id ORDER BY created_at LIMIT 1) AS egresos 
                     FROM clientprovider  AS cp 
                     INNER JOIN companies AS c ON c.id = cp.client_id OR c.id = cp.provider_id 
                     INNER JOIN operations AS o ON c.id = o.id_client OR c.id = o.id_provider
