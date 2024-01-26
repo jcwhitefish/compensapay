c1560a04507a96776902c27ec033b2d77023ac36
DRAGKOZ <skdkuriel@hotmail.com>
2024-01-26 10:21:25 -0600

    Fix date
    Signed-off-by: DRAGKOZ <skdkuriel@hotmail.com>

--- application/views/conciliaciones.php
+++ application/views/conciliaciones.php
@@ -554,7 +554,7 @@
 			input.addEventListener("change", function (e) {
 				let contenVar = $("#contentVariable");
 				if (e.target.id === "issuingWay") {
-					$("#conciliaDate").attr("disabled", false);
+					$("#conciliaDate").attr("disabled", true);
 					conciliateWay = 0;
 					let debit = "<div class=\"file-field input-field\" ><div class=\"file-path-wrapper\" style=\"width: 75%;margin-left: auto;float: left;\">" +
 						"<input class=\"file-path validate\" type=\"text\" placeholder=\"Sube tu nota de crédito en formato .xml\" disabled ></div>" +
@@ -563,7 +563,7 @@
 					contenVar.empty();
 					contenVar.append(debit);
 				} else {
-					$("#conciliaDate").attr("disabled", true);
+					$("#conciliaDate").attr("disabled", false);
 					conciliateWay = 1;
 					contenVar.empty();
 					$.ajax({
