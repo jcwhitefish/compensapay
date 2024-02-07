7fb87dcc0b5f83dff62bf22152cc1448b45e4947
DRAGKOZ <skdkuriel@hotmail.com>
2024-02-07 11:27:01 -0600

    Muestrario de notificaciones
    Signed-off-by: DRAGKOZ <skdkuriel@hotmail.com>

--- application/controllers/Notificaciones.php
+++ application/controllers/Notificaciones.php
@@ -1,46 +1,48 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-class Notificaciones extends MY_Loggedin{
-
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/userguide3/general/urls.html
-	 */
-	function __construct()
-	{
-
-	  parent:: __construct();
-	  $this->load->model('Notification_model'); 
-	}
-
-	public function index(){
-		$dato = array(
-			"noti" => $this->Notification_model->verNotificaciones(),
-			"dump" => 0
-		);
-
-		$data['main'] = $this->load->view('notificaciones', $dato, true);
-		$this->load->view('plantilla', $data);
-	}
+	defined ( 'BASEPATH' ) or exit( 'No direct script access allowed' );
 	
-	public function updatenot($idnoti){
-		$dato = array(
-			"noti" => $this->Notification_model->updateNoti($idnoti)
-		);
+	class Notificaciones extends MY_Loggedin {
+		/**
+		 * Index Page for this controller.
+		 *
+		 * Maps to the following URL
+		 *        http://example.com/index.php/welcome
+		 *    - or -
+		 *        http://example.com/index.php/welcome/index
+		 *    - or -
+		 * Since this controller is set as the default controller in
+		 * config/routes.php, it's displayed at http://example.com/
+		 *
+		 * So any other public methods not prefixed with an underscore will
+		 * map to /index.php/welcome/<method_name>
+		 * @see https://codeigniter.com/userguide3/general/urls.html
+		 */
+		function __construct () {
+			
+			parent:: __construct ();
+			$this->load->model ( 'Notification_model' );
+		}
+		public function index () {
+			$dato = [
+				"noti" => $this->Notification_model->verNotificaciones (),
+				"dump" => 0,
+			];
+			$data[ 'main' ] = $this->load->view ( 'notificaciones', $dato, TRUE );
+			$this->load->view ( 'plantilla', $data );
+		}
+		public function updatenot ( $idnoti ) {
+			$dato = [
+				"noti" => $this->Notification_model->updateNoti ( $idnoti ),
+			];
+			$this->load->view ( 'notificacion', $dato );
+		}
 		
-		$this->load->view('notificacion', $dato);
+		public function ShowAlerts (){
+			$dato = [
+				"noti" => $this->Notification_model->verNotificaciones (),
+				"dump" => 0,
+			];
+			$data[ 'main' ] = $this->load->view ( 'alerts', $dato, TRUE );
+			$this->load->view ( 'plantilla', $data );
+		}
 	}
-
-}
