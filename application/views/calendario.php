f9d423ed5127e30fc06820a2f22900f86da3e477
DRAGKOZ <skdkuriel@hotmail.com>
2024-02-08 15:15:47 -0600

    fix action
    Signed-off-by: DRAGKOZ <skdkuriel@hotmail.com>

--- application/views/calendario.php
+++ application/views/calendario.php
@@ -24,10 +24,10 @@
         </div>
         <div class="col l2 center-align">
             <div class="row">
-                <div class="col s12"><a @click="cambiarSatusBoton('p')" :class="`btnLeyenda btn-small  ${vProgramed == false ? 'transparent black-text' : 'programed'}`">Programadas</a></div>
+                <div class="col s12"><a :class="`btnLeyenda btn-small  ${vProgramed == false ? 'transparent black-text' : 'programed'}`">Programadas</a></div>
             </div>
             <div class="row">
-                <div class="col s12"><a @click="cambiarSatusBoton('v')" :class="`btnLeyenda btn-small  ${vOverdue == false ? 'transparent black-text' : 'overdue'}`">Vencidas</a></div>
+                <div class="col s12"><a  :class="`btnLeyenda btn-small  ${vOverdue == false ? 'transparent black-text' : 'overdue'}`">Vencidas</a></div>
             </div>
         </div>
     </div>
@@ -202,11 +202,6 @@
             };
 
             const cambiarSatusBoton = (boton) => {
-                if(boton == 'p'){
-                    vProgramed.value = !vProgramed.value;
-                }else{
-                    vOverdue.value = !vOverdue.value;
-                }
                 colocarOperaciones();
             }
 
