8f59cdfeca2ea4c2c37c2c4add36077e838c9031
DRAGKOZ <skdkuriel@hotmail.com>
2024-03-08 11:13:36 -0600

    Fix mail
    Signed-off-by: DRAGKOZ <skdkuriel@hotmail.com>

--- application/helpers/notifications_helper.php
+++ application/helpers/notifications_helper.php
@@ -54,10 +54,64 @@ con los datos correctos.";
 				$body = "Te informamos que la operaci&oacute;n <strong>" . $args[ 'operationNumber' ] . "</strong>. ha sido ejecutada con &eacute;xito";
 				$title = 'Operación realizada';
 				break;
+			case 8:
+				$body = "Te informamos que la operación <strong>{$args['operationNumber']}</strong> ha sido rechazada debido a: {$args['reason']}";
+				$title = "Operación rechazada";
+				break;
+			case 9:
+				$body = "Te informamos que la operaci&oacute;n <strong>{$args['operationNumber']}</strong> ha cambiado de estado a
+<strong>{$args['state']}</strong>.";
+				$title = "Operación {$args['state']}";
+				break;
+			case 10:
+				$body = "Te informamos que se hizo un dep&oacute;sito a tu cuenta clabe en solve por $ {$args['amount']} MXN.
+Debido a que no pudimos identificar el n&uacute;mero de operaci&oacute;n en la referencia, se hizo la devoluci&oacute;n del monto
+al cliente en su totalidad, por lo cual se ver&aacute; reflejado en tus movimientos y estados de cuenta.";
+				$title = "Devolución de pago";
+				break;
+			case 11:
+				$body = "Te informamos que se hizo un dep&oacute;sito a tu cuenta clabe en solve por $ {$args['amount']} MXN.
+Debido a que el monto transferido no corresponde a la operaci&oacute;n <strong>{$args['operationNumber']}</strong>, se hizo la
+devoluci&oacute;n del monto al cliente en su totalidad, por lo cual se ver&aacute; reflejado en tus movimientos y estados de cuenta.
+<br>Estaremos enviando notificaci&oacute;n de esto a tu cliente <strong>{$args['client']}</strong> para que vuelva a hacer el pago
+con los datos correctos.";
+				$title = "Devolución de pago";
+				break;
+			case 12:
+				$body = "Te informamos que tu cliente <strong>{$args['client']}</strong>, ha solicitado que cargues una factura para crear una
+operaci&oacute;n por la siguiente raz&oacute;n: {$args['reason']}.";
+				$title = "Solicitud de factura";
+				break;
 			case 13:
 				$url = base_url ( '/Conciliaciones' );
 				$body = "Te informamos que la operaci&oacute;n <strong>{$args['operationNumber']}</strong> ha sido creada por tu proveedor,
 es necesario validarla ingresando en el m&oacute;dulo de <strong>conciliaciones</strong> dando clic <a href='$url'>aquí</a>";
+				$title = "Operación creada";
+				break;
+			case 14:
+				$body = "Te informamos que tu cliente <strong>{$args['client']}</strong>, te ha invitado a que te registres como su proveedor
+en la plataforma Solve para ayudar a conciliar sus deudas recíprocas.";
+				$title = "Invitación a Solve";
+				break;
+			case 15:
+				$body = "Te informamos que tu cliente <strong>{$args['client']}</strong>, ha aprobado tu solicitud para que te registres
+como su proveedor en la plataforma Solve.";
+				$title = "Solicitud de registro aprobada";
+				break;
+			case 16:
+				$body = "Te informamos que tu proveedor <strong>{$args['provider']}</strong>, ha cargado nuevas facturas.";
+				$title = "Facturas cargadas";
+				break;
+			case 17:
+				$body = "Te informamos que la transacción de la operación <strong>{$args['operationNumber']}</strong> ha sido devuelta a su
+banco de origen debido a {$args['reason']}. Te recomendamos revisar los detalles de la operación. ";
+				$title = "Operación creada";
+				break;
+			case 18:
+				$url = base_url ();
+				$body = "Te informamos que tu proveedor {$args['client']}, te ha invitado a que te registres como su cliente en
+			<a href='$url' target='_blank'>solve.com.mx</a> para ayudar a conciliar sus deudas recíprocas. Da clic
+			<a href='{$args['url']}' target='_blank'>aquí</a> para revisar y aceptar su solicitud ";
 				$title = "Operación creada";
 				break;
 			case 100:
